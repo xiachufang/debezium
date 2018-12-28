593b365d199ac6062d3041c71ac9777802086d80
cadl <ctrlaltdeleteliu@gmail.com>
2018-12-28 15:26:29 +0800

    fix lint

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSnapshotConnector.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSnapshotConnector.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.connector.mysql;
 
 import org.apache.kafka.common.config.Config;
@@ -11,40 +16,40 @@ import java.util.List;
 import java.util.Map;
 
 public class MySqlSnapshotConnector extends SourceConnector {
-  private Map<String, String> props;
+    private Map<String, String> props;
 
-  @Override
-  public String version() {
-    return Module.version();
-  }
+    @Override
+    public String version() {
+        return Module.version();
+    }
 
-  @Override
-  public void start(Map<String, String> props) {
-    this.props = props;
-  }
+    @Override
+    public void start(Map<String, String> props) {
+        this.props = props;
+    }
 
-  @Override
-  public void stop() {
-    this.props = null;
-  }
+    @Override
+    public void stop() {
+        this.props = null;
+    }
 
-  @Override
-  public Class<? extends Task> taskClass() {
-    return MySqlSnapshotConnectorTask.class;
-  }
+    @Override
+    public Class<? extends Task> taskClass() {
+        return MySqlSnapshotConnectorTask.class;
+    }
 
-  @Override
-  public List<Map<String, String>> taskConfigs(int maxTasks) {
-    return props == null ? Collections.emptyList() : Collections.singletonList(new HashMap<String, String>(props));
-  }
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        return props == null ? Collections.emptyList() : Collections.singletonList(new HashMap<String, String>(props));
+    }
 
-  @Override
-  public ConfigDef config() {
-    return MySqlSnapshotConnectorConfig.configDef();
-  }
+    @Override
+    public ConfigDef config() {
+        return MySqlSnapshotConnectorConfig.configDef();
+    }
 
-  @Override
-  public Config validate(Map<String, String> connectorConfigs) {
-    return super.validate(connectorConfigs);
-  }
+    @Override
+    public Config validate(Map<String, String> connectorConfigs) {
+        return super.validate(connectorConfigs);
+    }
 }
--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSnapshotConnectorTask.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSnapshotConnectorTask.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.connector.mysql;
 
 import io.debezium.annotation.NotThreadSafe;
@@ -10,88 +15,91 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 @NotThreadSafe
 public final class MySqlSnapshotConnectorTask extends BaseSourceTask {
-  private volatile MySqlTaskContext taskContext;
-  private volatile SnapshotReader snapshotReader;
-  private MySqlSnapshotConnectorConfig config;
-  private static final Logger log = LoggerFactory.getLogger(MySqlSnapshotConnectorTask.class);
+    private volatile MySqlTaskContext taskContext;
+    private volatile SnapshotReader snapshotReader;
+    private MySqlSnapshotConnectorConfig config;
+    private static final Logger log = LoggerFactory.getLogger(MySqlSnapshotConnectorTask.class);
 
-  @Override
-  public synchronized void start(Configuration config) {
-    this.config = new MySqlSnapshotConnectorConfig(config);
-    this.taskContext = new MySqlTaskContext(config);
+    @Override
+    public synchronized void start(Configuration config) {
+        this.config = new MySqlSnapshotConnectorConfig(config);
+        this.taskContext = new MySqlTaskContext(config);
 
-    if (this.config.snapshotStatusFile == null) {
-      throw new ConnectException("snapshot.status.file is empty");
+        if (this.config.snapshotStatusFile == null) {
+            throw new ConnectException("snapshot.status.file is empty");
+        }
+
+        log.info("snapshot init, path: " + this.config.snapshotStatusFile);
+        Path path = Paths.get(this.config.snapshotStatusFile);
+
+        if (Files.exists(path)) {
+            log.error("snapshot status exist");
+            throw new ConnectException("snapshot status exist");
+        }
+
+
+        try {
+            log.info("snapshot start, path: " + this.config.snapshotStatusFile);
+            Files.createDirectories(path.getParent());
+            Files.write(path, "started".getBytes());
+        } catch (IOException e) {
+            throw new ConnectException(e.toString());
+        }
+
+        this.taskContext.start();
+        if (taskContext.historyExists()) {
+            throw new ConnectException("schema history exist");
+        }
+        taskContext.initializeHistoryStorage();
+
+        snapshotReader = new SnapshotReader("snapshot", taskContext);
+        snapshotReader.generateInsertEvents();
+        snapshotReader.uponCompletion(this::completReaders);
+        snapshotReader.initialize();
+        snapshotReader.start();
     }
 
-    log.info("snapshot init, path: " + this.config.snapshotStatusFile);
-    Path path = Paths.get(this.config.snapshotStatusFile);
-
-    if (Files.exists(path)) {
-      log.error("snapshot status exist");
-      throw new ConnectException("snapshot status exist");
+    protected void completReaders() {
+        Path path = Paths.get(this.config.snapshotStatusFile);
+        log.info("snapshot finish, path: " + this.config.snapshotStatusFile);
+        try {
+            Files.write(path, "completed".getBytes(), StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            throw new ConnectException(e.toString());
+        }
     }
 
+    @Override
+    public List<SourceRecord> poll() throws InterruptedException {
+        if (snapshotReader == null) {
+            return null;
+        }
 
-    try {
-      log.info("snapshot start, path: " + this.config.snapshotStatusFile);
-      Files.createDirectories(path.getParent());
-      Files.write(path, "started".getBytes());
-    } catch (IOException e) {
-      throw new ConnectException(e.toString());
+        return snapshotReader.poll();
     }
 
-    this.taskContext.start();
-    if (taskContext.historyExists()) {
-      throw new ConnectException("schema history exist");
-    }
-    taskContext.initializeHistoryStorage();
-
-    snapshotReader = new SnapshotReader("snapshot", taskContext);
-    snapshotReader.generateInsertEvents();
-    snapshotReader.uponCompletion(this::completReaders);
-    snapshotReader.initialize();
-    snapshotReader.start();
-  }
-
-  protected void completReaders() {
-    Path path = Paths.get(this.config.snapshotStatusFile);
-    log.info("snapshot finish, path: " + this.config.snapshotStatusFile);
-    try {
-      Files.write(path, "completed".getBytes(), StandardOpenOption.WRITE);
-    } catch (IOException e) {
-      throw new ConnectException(e.toString());
-    }
-  }
-
-  @Override
-  public List<SourceRecord> poll() throws InterruptedException {
-    if (snapshotReader == null) {
-      return null;
+    @Override
+    public void stop() {
+        if (snapshotReader != null) {
+            snapshotReader.stop();
+        }
     }
 
-    return snapshotReader.poll();
-  }
-
-  @Override
-  public void stop() {
-    if (snapshotReader != null) {
-      snapshotReader.stop();
+    @Override
+    public String version() {
+        return Module.version();
     }
-  }
 
-  @Override
-  public String version() {
-    return Module.version();
-  }
-
-  @Override
-  protected Iterable<Field> getAllConfigurationFields() {
-    return MySqlSnapshotConnectorConfig.ALL_FIELDS;
-  }
+    @Override
+    protected Iterable<Field> getAllConfigurationFields() {
+        return MySqlSnapshotConnectorConfig.ALL_FIELDS;
+    }
 }
